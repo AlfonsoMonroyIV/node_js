bde30e2867e2980b9f54ee0b315f9ba84e31bfaf
Alfonso Monroy <alfonso.monroy.ti@gmail.com>
2019-02-16 17:05:19 -0600

    testing http module 1

---
+++ http_test.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer(function(req,res){
+	res.writeHead(200,{'Content-type':'text/html'});
+	res.write('<h1>Hello World from NodeJS </h1>');
+	res.end();
+});
+
+
+server.listen(3000,function(){
+	console.log('Server on port 3000');
+});
